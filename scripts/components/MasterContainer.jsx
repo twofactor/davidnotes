73028b66aa47a31d0cb06f9a6c7225b44860eff9
Tina Zheng <tinazheng@berkeley.edu>
2016-12-31 16:32:45 -0500

    configured webpack for react + used node version of firebase

---
+++ scripts/components/MasterContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+class MasterContainer extends React.Component {
+  render() {
+    return <h1>Hello Worasdfasdfld</h1>
+  }
+}
+
+ReactDOM.render(<MasterContainer />, document.getElementById('react-content'));

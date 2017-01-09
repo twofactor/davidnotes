d586d4c4ca86696188d0fcdaad32fd7c07b60c20
David Bui <david@Davids-MacBook-Pro.local>
2017-01-08 16:33:14 -0800

    i forgot what i changed

---
+++ scripts/components/MemeEditor.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+  Editor,
+  createEditorState,
+  ImageSideButton,
+  Block,
+  addNewBlock,
+} from 'medium-draft';
+
+import {
+  convertToRaw,
+  convertFromRaw,
+} from 'draft-js'
+
+export default class UnicornEditor extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editorState: createEditorState(), // for empty content
+    };
+
+    this.sideButtons = [{
+      "title": "Image",
+      "component": ImageSideButton
+    }];
+
+    /*
+    this.state = {
+      editorState: createEditorState(data), // if you have initial data
+    };
+    */
+
+    this.onChange = (editorState) => {
+      this.setState({ editorState });
+      var editorData = convertToRaw(this.state.editorState.getCurrentContent());
+      console.log(editorData);
+    };
+  }
+
+  componentDidMount() {
+    this.refs.editor.focus();
+  }
+
+  render() {
+    const { editorState } = this.state;
+    return (
+      <Editor
+        ref="editor"
+        editorState={editorState}
+        onChange={this.onChange}
+        sideButtons={this.sideButtons}
+         />
+    );
+  }
+};
